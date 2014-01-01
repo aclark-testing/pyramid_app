c30f195dfd0725de030aeb6a001befc1df9cd56c
Alex Clark <aclark@aclark.net>
2013-12-31 20:51:43 -0500

    Add entry point

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
+    entry_points={
+        'paste.app_factory': 'main=pyramid_app:main',
+    },
     install_requires=[
         'pyramid',
+        'waitress',
     ],
     name='pyramid_app',
 )
